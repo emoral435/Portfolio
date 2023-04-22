3d1d48056d6f13887c7f6eb4a079d5ce125ba08a
Eduardo Morales <emoral435@gmail.com>
2023-04-22 18:21:11 -0500

    Changing StickyNav to fit needs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useTheme } from "@emotion/react"
 import StickyNav from "./components/StickyNav/StickyNav"
+import Box from '@mui/material/Box';
 
 function App() {
+  const theme = useTheme()
+  
   return (
-    <>
-      <StickyNav />
-    </>
+    <div style={{
+      backgroundColor: theme.palette.background.default,
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+      }}>
+      <Box>
+        <StickyNav />
+      </Box>
+    </div>
   )
 }
 
