0b4ccbfc36a58ad2af258446e8035ef32ffbe834
Eduardo Morales <emoral435@gmail.com>
2024-02-20 21:48:24 -0600

    feat: introduced MUI icons to footer and header
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -4,48 +4,42 @@ import { Box } from "@mui/material"
 import { useTheme } from "@mui/material/styles";
 import CSLOGO from '/public/CSLOGO.svg'
 import FLAMES from '/src/assets/flames.svg'
-import PROFILE from '/src/assets/profilePic.jpg'
+import PROFILEGIF from '/src/assets/profileGif.gif'
+import { introInfo } from "../../services/constants";
 
 const Introduction = () => {
 const theme = useTheme()
 
-    const floatingText = "Hey there"
-    const welcome = "My name is Eduardo, nice to meet you!"
-    const interest = "I am interested in designing websites, apps, games, and overall enjoy development of any sort!"
-    const position = "Student Software Engineer @ Nextcloud"
-    const major = "Computer Science"
-    const uni = "University of Illinois Chicago"
-
   return (
     <Box component={'section'} sx={{ display: 'flex', flexDirection: {xs: 'column', md: 'row'}, gap: {xs: 2, md: 15}, justifyContent: 'center', alignItems: 'center' }}>
         <Box component={'section'} sx={{display: 'flex', gap: {xs: '1rem', md:'3rem'}}}>
-            <Links whiteMode={false}/>
+            <Links inFooter={false}/>
             <Box sx={{width: {xs: '15rem', md: '17rem', lg: '25rem'}, height: {xs: '15rem', md: '17rem', lg: '25rem'}}}>
-                <img src={PROFILE} alt="" className="profile-pic"/>
+                <img src={PROFILEGIF} alt="" className="profile-pic"/>
             </Box>
         </Box>
         <Box component={'section'} sx={{maxWidth: 'min-content'}}>
             <Box sx={{paddingLeft: 3 ,textAlign: 'center', whiteSpace: 'nowrap' ,fontWeight: 700 ,fontSize: {xs: '3.7rem',sm: '4.5rem', md: '6.5rem'}}}>
-                <ReactTyped strings={[floatingText]} typeSpeed={110} className="typed" />
+                <ReactTyped strings={[introInfo.floatingText]} typeSpeed={110} className="typed" />
             </Box>
             <Box component={'h2'} sx={{textAlign: 'center' ,color: theme.palette.text.secondary, fontWeight: 700, fontSize: {xs: '1rem', sm: '1.22rem', md: '1.7rem'}}}>
-                {welcome}
+                {introInfo.welcome}
             </Box>
             <Box component={'h2'} sx={{margin: '1rem' , textAlign: 'center' ,color: theme.palette.text.secondary, fontWeight: 700, fontSize: {xs: '1rem', sm: '1.22rem', md: '1.7rem'}}}>
-             {interest}
+             	{introInfo.interest}
             </Box>
             <Box component={'h2'} sx={{ display: 'flex', flexDirection: 'column', gap: 1 ,color: theme.palette.text.secondary, fontSize: {xs: '.6rem' ,sm: '1rem', md: '1.1rem'}}}>
                 <Box component={'div'} sx={{display: 'flex', gap: '6px', justifyContent: 'space-around', alignItems: 'center'}}>
-                    <Box component={'h3'}>{position}</Box>
+                    <Box component={'h3'}>{introInfo.position}</Box>
                 </Box>
                 <Box component={'div'} sx={{display: 'flex', flexDirection: {xs: 'column', md: 'row'}, gap: '6px', justifyContent: 'center', alignItems: 'center', whiteSpace: 'nowrap'}}>
                     <Box className="intro-img-container" >
                         <img src={CSLOGO} alt="" className="intro-img" />
-                        <Box component={'h3'}>{major}</Box>
+                        <Box component={'h3'}>{introInfo.major}</Box>
                     </Box>
                     <Box className="intro-img-container" >
                         <img src={FLAMES} alt="" className="intro-img"/>
-                        <Box component={'h3'}>{uni}</Box>
+                        <Box component={'h3'}>{introInfo.uni}</Box>
                     </Box>
                 </Box>
             </Box>
--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,24 +1,33 @@
 import { Box } from "@mui/material"
-import GH from '/src/assets/github.svg'
-import GHW from '/src/assets/githubWhite.svg'
-import LI from '/src/assets/linkedIn.svg'
-import LIW from '/src/assets/linkedInWhite.svg'
-import MAIL from '/src/assets/mail.svg'
-import repo from '/src/assets/repo.svg'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ReplyIcon from '@mui/icons-material/Reply';
 
 interface Props {
-  whiteMode: boolean
+  inFooter: boolean
 }
 
-const Links = ({whiteMode} : Props) => {
-  const bottomRedirect = whiteMode ? "https://github.com/emoral435/Portfolio" : "mailto: emoral435@gmail.com"
+const Links = ({inFooter} : Props) => {
+  const bottomRedirect = inFooter ? "https://github.com/emoral435/Portfolio" : "mailto: emoral435@gmail.com"
 
-  const direction = whiteMode ? 'row' : 'column'
+  const getClassName = inFooter ? "intro-sidefooter-img" : "intro-side-img"
+
+  const direction = inFooter ? 'row' : 'column'
   return (
     <Box component={'nav'} sx={{display: 'flex', flexDirection: {xs: direction, md:'column'}, gap: 4, justifyContent: 'space-around', alignItems: 'center'}}>
-      <a href="https://www.linkedin.com/in/emswe/" target="_blank">{whiteMode ? <img src={LIW} alt="" className="intro-sidefooter-img" /> : <img src={LI} alt="" className="intro-side-img" />}</a>
-      <a href="https://github.com/emoral435" target="_blank">{whiteMode ? <img src={GHW} alt="" className="intro-sidefooter-img" /> : <img src={GH} alt="" className="intro-side-img" />}</a>
-      <a href={bottomRedirect} target="_blank">{whiteMode ? <img src={repo} alt="" className="intro-sidefooter-img" /> : <img src={MAIL} alt="" className="intro-side-img" />}</a>
+      <a href="https://www.linkedin.com/in/emswe/" target="_blank">
+		<LinkedInIcon className={`${getClassName}`}/>	
+	  </a>
+      <a href="https://github.com/emoral435" target="_blank">
+			<GitHubIcon className={`${getClassName}`}/>
+	  </a>
+      <a href={bottomRedirect} target="_blank">
+	 	{
+			inFooter
+			? <GitHubIcon className={`${getClassName}`}/>
+			: <ReplyIcon className={`${getClassName}`}/>
+		}
+	  </a>
     </Box>
     )
 }
