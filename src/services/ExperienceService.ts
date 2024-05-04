4b5984050649fdab171d0861a0ee29f6612b1f70
Eduardo Morales <emoral435@gmail.com>
2024-05-03 23:56:45 -0500

    feat: changed bullet styling and updated bullets
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- src/services/ExperienceService.ts
+++ src/services/ExperienceService.ts
@@ -49,19 +49,38 @@ Enova.extraURL = "https://nextcloud.com/team/"
 
 // Nextcloud experience
 const ncBullets = [
-    'Waiting to start!',
+    'Developed an API in PHP to extend legacy MySQL databases to support storing file metadata,' + 
+    ' resulting in version history and version author being displayed for each file, with support to display different file metadata in the future',
+
+    'Increased product\'s accessibility functionality to 10+ Vue.js components by adding focus-traps and keyboard listeners,' + 
+    ' leading to a BITV certification, garnering new contracts for customers who wanted to use the product within Universities',
+
+    'Implemented a \'Personal Files\' view by filtering out shared resources returned by WebDAV queries made in TypeScript,' + 
+    ' enabling users to be able to differentiate between shared or personal files and folders',
+
+    'Refactored menu options in Vue.js to re-render every 30 minutes, ensuring menu displays accurate time-reliant actions',
+
+    'Collaborated with 12 engineers, maintaining CI/CD on an open-source content-collaboration product with 2.2M users',
+
 ]
 const Nextcloud = new Company("Nextcloud", "Stuggart, Germany", "Part-time Software Engineer Intern", "Nov. 2023", "May 2024", ncBullets)
 Nextcloud.iconSrc = NC
 Nextcloud.url = "http://www.nextcloud.com"
 Nextcloud.extraURL = "https://nextcloud.com/team/"
 
-// Data Analyst experience
+// Data Analyst Researcher experience
 const uicInternship = [
-	'Examined 20 gigabytes of patent data sets spanning over 3 decades, finding relations in patents and job trends',
-    'Succeeded in finding the 10 most valued developer skills from online job posting documents',
-    'Showcased a 12 percent increase in similarities between job descriptions and patent details 2 years after being granted by applying SciKitLearn word embedding methods.'
+	'Examined 20 gigabytes of patent data spanning over 3 decades, using Pandas to create dataframes relating to remote job' + 
+    ' positions, yielding a finding of the 10 most valued developer skills and traits from online job postings',
+
+    'Discovered an average 12% increase yearly since 2019 in new remote job positions, using Word2Vec to find associated' + 
+    ' documents created from patent and job dataframes, concluding that granted patents from 2 years prior led to the increase',
+
+    'Researched the relationship between unemployment and the number of yearly granted patents within the U.S. using Python,' + 
+    ' finding a positive 1:1 correlation between unemployment percentage and the number of granted patents',
+    
 ]
+
 const DataAnalyst = new Company("University of Illinois Chicago", "Chicago, Illinois", "Full-time Data Analyst", "June 2023", "August 2023", uicInternship)
 DataAnalyst.url = "https://engineering.uic.edu/"
 DataAnalyst.iconSrc = UIC
